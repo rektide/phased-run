2cf98f85c39df43910c17d3431ad761d1b699873
rektide de la faye <rektide@voodoowarez.com>
2018-10-12 22:35:18 -0400

    Start parser.js to, like parse, get root & modifier
    Checks for valid root via base.js
    Derives the modifier value via prefix.js

---
+++ parser.js
@@ -0,0 +1,12 @@
+import base from "./base.js"
+import prefix from "./prefix.js"
+
+export function parser( phases){
+	const phaseFinder= base( phases)
+	function parse( target){
+		
+	}
+	return parse
+}
+export default parser
+
