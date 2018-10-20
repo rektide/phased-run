83a3f030212001dcd494981be35af53be60f6abf
rektide de la faye <rektide@voodoowarez.com>
2018-10-20 16:00:02 -0400

    Colletion
    Data-structure to hold a set of runs

---
+++ collection.js
@@ -0,0 +1,54 @@
+import Splicer from "splicer"
+import parse from "./parse.js"
+
+const
+  $collection= Symbol.for( "PhaseRunner:collection"),
+  $keys= Symbol.for( "PhaseRunner:keys"),
+  $map= Symbol.for( "PhaseRunner:map")
+
+export const Symbols= {
+	collection: $collection,
+	keys: $keys
+}
+
+export class PhaseRunnerCollection extends Splicer{
+	constructor({ phases, findPhase }={}, ...phases){
+		this[ $keys]= null
+		this[ $map]= new Map()
+	}
+	splice( i, del, ...added){
+		const
+		  map= this[ $map],
+		  end= i+ del
+		for( let j= i; j< end; ++j){
+			const phase= findPhase( 
+			this.keys(
+		}
+		for( let k of added){
+			const
+			  phase= this.findPhase( k),
+			  value= this.parse( phase),
+			  existing= map.get( value)
+			if( !existing){
+				map.set( value, [ phase])
+			}else{
+				existing.push( phase)
+			}
+		}
+		if( del>= 0|| added&& added.length){
+			this[ $keys]= null
+		}
+	}
+	get keys(){
+		const current= this[ $keys]
+		if( current){
+			return current
+		}
+		const keys= Array.from( this[ $map].keys())
+		return this[ $keys]= keys.sort()
+	}
+	*[ Symbol.iterator](){
+		
+		
+	}
+}
