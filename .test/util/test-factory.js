9e4b3f91e409d071f03f5805c1e3099d89f9fb91
rektide de la faye <rektide@voodoowarez.com>
2018-10-20 16:14:13 -0400

    Add a test case generator & test matrix generator.

--- .test/util/test-factory.js
+++ .test/util/test-factory.js
@@ -5,4 +5,38 @@ export function testFactory( name, fn){
 		tape( name, fn)
 	}
 }
+
+function expandArgs( caseName, args){
+	if( caseName){
+		return caseName
+	}
+	return args
+	  .map( arg=> arg instanceof Object? JSON.stringify( args): arg)
+	  .join(":")
+}
+
+export function simpleTestFactory( name, fn, { args, expected, caseName}){
+	const
+	  displayName= `${name}:${expandArgs( caseName, args)}`,
+	  testCase= ({
+		[ displayName]: function(){
+			test( displayName, function( t){
+				const
+				  areArgsArray= testCase.args&& testCase.args[ Symbol.iterator],
+				  argArray= testCase.args&& (areArgsArray? testCase.args: [ testCase.args])|| [],
+				  ran= fn( ...argsArray)
+				t.deepEqual( ran, testCase.expected, displayName)
+				t.end()
+			})
+		}
+	  })[ displayName]
+	testCase.expected= expected
+	testCase.args= args
+	return testCase
+}
+
+export function testMatrixFactory( name, fn, cases){
+	return cases.map( _case=> simpleTestFactory( name, fn, _case))
+}
+
 export default testFactory
