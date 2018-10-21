95543875ad1045735ee2e3ec86bcbd08fcc7c603
rektide de la faye <rektide@voodoowarez.com>
2018-10-21 16:12:09 -0400

    Try to add a delete to collection.

--- collection.js
+++ collection.js
@@ -13,16 +13,58 @@ export const Symbols= {
 
 export class PhaseRunnerCollection extends Splicer{
 	constructor({ phases, findPhase }={}, ...phases){
-		this[ $keys]= null
-		this[ $map]= new Map()
+		this[ $map]= new Map() // phase-number => array of values for that phase
+		this[ $keys]= null // sorted array of phase-numbers
 	}
 	splice( i, del, ...added){
-		const
-		  map= this[ $map],
-		  end= i+ del
-		for( let j= i; j< end; ++j){
-			const phase= findPhase( 
-			this.keys(
+		const map= this[ $map]
+		let rekey= false
+		if( del){
+			let
+			  skip= i // we have to advance to advance this many steps
+			  remove= del, // have this many left to remove
+			  major= 0, // which key we are on
+			  minor= 0, // which element in the current array we are on
+			  key,
+			  arr
+			function update(){
+				key= this[ $keys][ major],
+				arr= this[ $map].get( key)
+			}
+			update()
+			// advance through skip
+			while( arr.length< skip){
+				skip-= arr.length
+				major++
+				update()
+			}
+			minor= skip
+
+			// delete
+			while( arr&& remove> 0){
+				// remove whole arrays as fast as possible
+				while( minor=== 0&& arr.length<= remove){
+					this[ $map].delete( key)
+					rekey= true
+					remove-= arr.length
+					major++
+					update()
+				}
+				if( !arr){
+					break
+				}
+				// remove pieces
+				const
+				  capacity= arr.length- minor,
+				  removal= Math.min( remove, capacity)
+				arr.splice( minor, removal)
+				remove-= removal
+				if( remove){
+					major++
+					minor= 0
+					update()
+				} // else { minor += removal } // unnecessary since we're done
+			}
 		}
 		for( let k of added){
 			const
@@ -30,12 +72,13 @@ export class PhaseRunnerCollection extends Splicer{
 			  value= this.parse( phase),
 			  existing= map.get( value)
 			if( !existing){
+				rekey= true
 				map.set( value, [ phase])
 			}else{
 				existing.push( phase)
 			}
 		}
-		if( del>= 0|| added&& added.length){
+		if( rekey){
 			this[ $keys]= null
 		}
 	}
