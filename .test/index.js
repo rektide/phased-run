8a60e2521659b6a1953ca504e215facd65ac5138
rektide de la faye <rektide@voodoowarez.com>
2018-10-21 20:21:45 -0400

    Begin a collection test, testing deletes via splice.

--- .test/index.js
+++ .test/index.js
@@ -1,11 +1,20 @@
 import parse from "./parse.js"
 import base from "./base.js"
 import prefix from "./prefix.js"
+import collection from "./collection.js"
+
+export {
+	parse,
+	base,
+	prefix,
+	collection
+}
 
 export function all(){
 	parse()
 	base()
 	prefix()
+	collection()
 }
 export default all
 
