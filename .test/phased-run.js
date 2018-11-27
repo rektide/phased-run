3605d7b037d366a2fd6d4490d399c8c8ff6ff07c
rektide de la faye <rektide@voodoowarez.com>
2018-11-26 19:04:28 -0500

    Mkae test go.

--- .test/phased-run.js
+++ .test/phased-run.js
@@ -4,18 +4,25 @@ import PhasedRun from "../phased-run.js"
 
 const trafficLight= [ "green", "yellow", "red"]
 
-const addItems= test( "can add items into a phased-run", function( t){
-	const
-	  accum= 0,
-	  pr= new PhasedRun( trafficLight)
-	pr.install( "yellow", ()=> accum+= 1)
+const addItemsOoO= test( "can add items into a phased-run", function( t){
+	let accum= 0
+	const pr= new PhasedRun( trafficLight)
+	// inserting out of order
+	pr.install( "yellow", ()=> accum+= 2)
 	pr.install( "red", ()=> accum+= 3)
 	pr.install( "green", ()=> accum+= 1)
+	// but we expect them to run in order
+	pr[ 0]()
+	t.assert( accum, 1, "green runs")
+	pr[ 1]()
+	t.assert( accum, 3, "yellow")
+	pr[ 2]()
+	t.assert( accum, 6, "red")
 	t.end()
 })
 
 export function main(){
-	addItems()
+	addItemsOoO()
 	console.log("yes")
 }
 export default main
