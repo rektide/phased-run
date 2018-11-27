9ae9646b10d7c6f5c967936d4e63100e2ce9fc45
rektide de la faye <rektide@voodoowarez.com>
2018-11-26 19:43:09 -0500

    Ohh I thought things were working but tests were no good

--- .test/phased-run.js
+++ .test/phased-run.js
@@ -1,28 +1,33 @@
 import test from "./util/test-factory.js"
+import increments from "./fixture/increments.js"
 
 import PhasedRun from "../phased-run.js"
 
 const trafficLight= [ "green", "yellow", "red"]
 
-const addItemsOoO= test( "can add items into a phased-run", function( t){
-	let accum= 0
+const basicDemonstration= test( "can add items into a phased-run & it will sort them", function( t){
+	// create a value and some tools we can increment with
+	let { inc1, inc2, inc3}= increments()
 	const pr= new PhasedRun( trafficLight)
 	// inserting out of order
-	pr.install( "yellow", ()=> accum+= 2)
-	pr.install( "red", ()=> accum+= 3)
-	pr.install( "green", ()=> accum+= 1)
+	pr.install( "yellow", inc2)
+	t.equal( pr.length, 1, "has one time")
+	pr.install( "red", inc3)
+	t.equal( pr.length, 2, "has one time")
+	pr.install( "green", inc1)
+	t.equal( pr.length, 3, "has one time")
 	// but we expect them to run in order
-	pr[ 0]()
-	t.assert( accum, 1, "green runs")
-	pr[ 1]()
-	t.assert( accum, 3, "yellow")
-	pr[ 2]()
-	t.assert( accum, 6, "red")
+	t.equal( pr.phases[ 0], "green", "green first")
+	t.equal( pr[ 0], inc1, "green first")
+	t.equal( pr.phases[ 1], "yellow", "yellow second")
+	t.equal( pr[ 1], inc2)
+	t.equal( pr.phases[ 2], "red", "red last")
+	t.equal( pr[ 2], inc3, "yellow")
 	t.end()
 })
 
 export function main(){
-	addItemsOoO()
+	basicDemonstration()
 	console.log("yes")
 }
 export default main
