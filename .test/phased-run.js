c9d0cc4a782ec4e62e5e67bbe0fb68ccf8681b18
rektide de la faye <rektide@voodoowarez.com>
2018-12-08 19:06:30 -0500

    Rebuild atop self-sorting-array. Items must declare phase.
    Pretty big shift to usage of this library.
    
    Previously, I was using "install" to specify a phase for things being
    installed. Now, I rely on the thing to declare it's own phase. This
    change means we only need the array itself, where-as before phase
    had to be held in a secondary array.
    
    Originally I wanted to avoid the wrapper object, for performance/
    allocations reasons, but I found that I wanted to associate a symbol
    with each item in the array, and that made creating a wrapper object
    that held the phase & associated symbol more logical.

--- .test/phased-run.js
+++ .test/phased-run.js
@@ -6,43 +6,27 @@ import trafficLight from "./fixture/traffic.js"
 
 const basicDemonstration= test( "can add items into a phased-run & it will sort them", function( t){
 	// create a value and some tools we can increment with
-	let { getAccum, inc1, inc2, inc3}= increments()
 	const pr= new PhasedRun( trafficLight)
 	// inserting out of order
-	pr.install( inc2, "yellow")
-	pr.install( inc3, "red")
-	pr.install( inc1, "green")
+	pr.push({ phase: "yellow"})
+	pr.push({ phase: "red"})
+	pr.push({ phase: "green"})
 	t.equal( pr.length, 3, "has three items")
 
 	// but we expect them to run in order
-	t.equal( pr.state[ 0], "green", "green first")
-	t.equal( pr[ 0], inc1, "green/inc1 first")
-	pr[ 0]()
-	t.equal( getAccum(), 1, "+1 to 1")
-
-	t.equal( pr.state[ 1], "yellow", "yellow/inc2 second")
-	t.equal( pr[ 1], inc2)
-	pr[ 1]()
-	t.equal( getAccum(), 3, "+2 to 3")
-
-	t.equal( pr.state[ 2], "red", "red/inc3 last")
-	t.equal( pr[ 2], inc3, "yellow")
-	pr[ 2]()
-	t.equal( getAccum(), 6, "+3 to 6")
-
+	t.equal( pr[ 0].phase, "green", "green first")
+	t.equal( pr[ 1].phase, "yellow", "yellow/inc2 second")
+	t.equal( pr[ 2].phase, "red", "red/inc3 last")
 	t.end()
 })
 
 const insertAfter= test( "adding an item of identical value will add it to the end of the phase", function( t){
 	// create a value and some tools we can increment with
-	let { getAccum, inc1, inc2, inc3}= increments()
 	const pr= new PhasedRun( trafficLight)
-	pr.install( inc1, "yellow")
-	pr.install( inc2, "yellow")
-	pr[ 0]()
-	t.equal( getAccum(), 1, "+1 to 1")
-	pr[ 1]()
-	t.equal( getAccum(), 3, "+2 to 3")
+	pr.push({ phase: "yellow", mod: 10})
+	pr.push({ phase: "yellow", mod: 20})
+	t.equal( pr[ 0].mod, 10)
+	t.equal( pr[ 1].mod, 20)
 	t.end()
 })
 
--- phased-run.js
+++ phased-run.js
@@ -1,75 +1,22 @@
-import binarySearch from "binary-search"
-
+import SelfSortingArray, { _speciesHack} from "self-sorting-array"
 import Comparator from "./comparator.js"
 import NotImplemented from "./not-implemented.js"
 import { $state, $comparator} from "./symbol.js"
 
-let hackSpecies= 0
+function makeComparator( comparatorOrPhases){
+	if( comparatorOrPhases instanceof Function){
+		// looks like a comparator
+		return comapratorOrPhases
+	}
+	return Comparator( comparatorOrPhases)
+}
 
-export class PhasedRun extends Array{
-	constructor( comparatorOrPhases){
-		super()
-		Object.defineProperties( this, {
-			// store the comparator we use for binary search
-			[ $comparator]: {
-				value: Comparator( comparatorOrPhases)
-			},
-			// store state on each item
-			[ $state]: {
-				value: [],
-				writable: true
-			}
-		})
-	}
-	get comparator(){
-		return this[ $comparator]
-	}
-	get state(){
-		return this[ $state]
-	}
+export class PhasedRun extends SelfSortingArray{
 	static get [ Symbol.species](){
-		return hackSpecies? Array: PhasedRun
+		return _speciesHack? Array: PhasedRun
 	}
-
-	install( item, state){
-		const
-		  states= this[ $state],
-		  i= binarySearch( states, state, this[ $comparator]),
-		  abs= i< 0? -i- 1: i
-		hackSpecies++
-		Array.prototype.splice.call( this, abs, 0, item)
-		hackSpecies--
-		states.splice( abs, 0, state)
-		return abs
-	}
-
-	clone(){
-		const clone= new PhasedRun( this[ $comparator])
-		Array.prototype.splice.call( clone, 0, 0, this)
-		// hazard: we're using existing pieces!
-		clone[ $comparator]= this[ $comparator]
-		clone[ $state]= this[ $state]
-		return clone
-	}
-
-	// conventional mutations aren't applicable, since they don't declare a phase
-	concat(){
-		NotImplemented()
-	}
-	copyWithin(){
-		NotImplemented()
-	}
-	fill(){
-		NotImplemented()
-	}
-	push(){
-		NotImplemented()
-	}
-	unshift(){
-		NotImplemented()
-	}
-	splice(){
-		NotImplemented()
+	constructor( comparatorOrPhases, ...items){
+		super( makeComparator( comparatorOrPhases))
 	}
 }
 export default PhasedRun
